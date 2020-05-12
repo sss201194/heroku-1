5c2a37c17b41ea2a116f00f4485ed90e0b340a2e
sss201194 <47942465+sss201194@users.noreply.github.com>
2020-05-12 14:53:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 17:08:46 2020
+
+@author: sande
+"""
+
+import numpy as np
+from flask import Flask,request,jsonify,render_template
+import pickle
+
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    int_features=[int(x)for x in request.form.value()]
+    final_features = [np.array(int_features)]
+    prediction=model.predict(final_features)
+    
+    output=round(prediction[0],2)
+    
+    return render_template('index.html',prediction_text='Employee Salary should be {}'.format(output))
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
---
+++ model.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 16:51:50 2020
+
+@author: sande
+"""
+
+import numpy as numpy
+import pandas as pd
+import pickle
+
+dataset=pd.read_csv('Salary.csv')
+x=dataset.iloc[:,:2]
+y=dataset.iloc[:,-1]
+
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression
+
+regressor.fit(x,y)
+
+pickle.dump(regressor,open('model.pkl','wb'))
+
+model=pickle.load(open('model.pkl','rb'))
+
