384370600ec490df3755fe9f77f305a2395b9985
sss201194 <47942465+sss201194@users.noreply.github.com>
2020-05-17 11:54:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def predict():
     return render_template('index.html',prediction_text='Employee Salary should be {}'.format(output))
 
 if __name__=='__main__':
-    app.run(debug=True)
+   
     
